122f9b9a46e9e66f0cffb3c25b3b8738acb7eb5e
Kate <katyakychka@gmail.com>
2025-01-31 04:46:43 +0300

    Replacing System.out.println with a logger, prepared statement and changes written by SonarQube

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,32 +2,32 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.util.Collections;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
+        // no-op
     }
-
 
     @Override
     public void createUsersTable() {
-
+        // no-op
     }
 
     @Override
     public void dropUsersTable() {
-
+        // no-op
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        // no-op
     }
 
     @Override
     public void removeUserById(long id) {
-
+        // no-op
     }
 
     @Override
@@ -37,6 +37,6 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-
+        // no-op
     }
 }
